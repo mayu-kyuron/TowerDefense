572f32450fbaff346d1b9ebd33602f63ed413e21
KOHEI <m.k.tennis0626@gmail.com>
2018-08-15 13:01:00 +0900

    prefab作り

--- Assets/BattleTempScene/Scripts/CharaMonsterNoConst.cs
+++ Assets/BattleTempScene/Scripts/CharaMonsterNoConst.cs
@@ -18,4 +18,27 @@ public class CharaMonsterNoConst {
 
 	public const int SlimeNo = 1;
 	public const int GaitherNo = 2;
+    public const int SlimeRedNo = 3;
+    public const int GaitherWhiteNo = 4;
+    public const int ZonbieWhiteNo = 5;
+    public const int ZonbieGreenNo = 6;
+    public const int GoblinGreenNo = 7;
+    public const int GoblinBlackNo = 8;
+    public const int YetiWhiteNo = 9;
+    public const int YetiGreenNo = 10;
+    public const int IfreetRedNo = 11;
+    public const int IfreetGreenNo = 12;
+    public const int CentaurBrownNo = 13;
+    public const int CentaurBlackNo = 14;
+    public const int UnicornWhiteNo = 15;
+    public const int UnicornBlackNo = 16;
+    public const int OgreOrangeNo = 17;
+    public const int OgreGreenNo = 18;
+    public const int ChimeraPurpleNo = 19;
+    public const int ChimeraRedNo = 20;
+    public const int FairyYerrowNo = 21;
+    public const int FairyPurpleNo = 22;
+    public const int CarbuncleNo = 23;
+    public const int GolemNo = 24;
+    public const int AlrauneNo = 25;
 }
