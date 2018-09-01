ea2e937a1e66c6420633328dbf963a81b497b8e6
KOHEI <m.k.tennis0626@gmail.com>
2018-09-01 17:38:08 +0900

    ステータス、攻撃頻度修正

--- Assets/StatusScene/Scripts/StatusContents.cs
+++ Assets/StatusScene/Scripts/StatusContents.cs
@@ -11,22 +11,22 @@ public class StatusContents : MonoBehaviour {
     public const string FighterBStatus = "普通\nとても高い\n低い";
     public const string FighterBStatus2 = "複数\n速い\n20";
 
-    public const string FighterCStatus = "とても高い\nとても低い\n低い";
+    public const string FighterCStatus = "とても高い\nとても低い\nとても低い";
 	public const string FighterCStatus2 = "単体\n遅い\n15";
 
 	public const string WitchAStatus = "低い\n高い\nとても高い";
     public const string WitchAStatus2 = "単体\nとても速い\n15";
 
-    public const string WitchBStatus = "とても低い\nとても高い\nとても低い";
+    public const string WitchBStatus = "とても低い\nとても高い\n低い";
     public const string WitchBStatus2 = "複数\n普通\n22";
 
-    public const string HealerAStatus = "普通\n高い\n普通";
+    public const string HealerAStatus = "普通\n高い\n高い";
     public const string HealerAStatus2 = "単体\nとても遅い\n15";
 
     //public const string HealerBStatus = "低い\n高い\nとても遅い";
     //public const string HealerBStatus2 = "複数\nとても遅い\n20";
 
-    public const string HealerCStatus = "普通\n普通\n×";
+    public const string HealerCStatus = "普通\n普通\n普通";
     public const string HealerCStatus2 = "複数\nとても遅い\n20";
 
     //public const string GuardianAStatus = "ガーディアンAのステータス";
